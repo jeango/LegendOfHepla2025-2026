029cc151bacec26469caaac67ed557e99f7703c7
jeango <jdc@funtomata.com>
2025-10-05 16:11:40 +0200

    Update Example.cs

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -7,6 +7,7 @@ public class Example : MonoBehaviour
     
     void Start()
     {
+        //This is a dummy comment
         counter = 1;
         print("Hello " + personWeAreTalkingTo + "! My name is " + gameObject.name);
     }
