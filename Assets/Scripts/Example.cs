47d8aaee150dca4aa17c52056860f268f1400103
jeango <jdc@funtomata.com>
2025-10-05 16:35:35 +0200

    Initial Commit

--- Assets/Scripts/Example.cs
+++ Assets/Scripts/Example.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class Example : MonoBehaviour
 {
-    public string personWeAreTalkingTo;
+    public string message;
     private int counter;
-    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         counter = 1;
-        print("Hello " + personWeAreTalkingTo + "! My name is " + gameObject.name);
+        print(message);
     }
 
     // Update is called once per frame
