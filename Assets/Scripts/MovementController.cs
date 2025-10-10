10f825c957f3192f57f0b59c85989ac1342fe9bf
jeango <jdc@funtomata.com>
2025-10-10 08:14:05 +0200

    fin cours 7 oct

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementController : MonoBehaviour
 {
     public Vector3 movementDirection;
-    public float speed;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float movementSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += movementDirection.normalized * Time.deltaTime * speed;
+        Move(movementDirection, movementSpeed);
+    }
+
+    private void Move(Vector3 direction, float speed)
+    {
+        transform.position += direction.normalized * (Time.deltaTime * speed);
+    }
+
+    public void SetDirectionFromInput(InputAction.CallbackContext context)
+    {
+        movementDirection = context.ReadValue<Vector2>();
     }
 }
