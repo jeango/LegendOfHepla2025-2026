e784f9f8b956e0e9cb29d6ba95694eb0d240eed4
jeango <jdc@funtomata.com>
2025-11-18 15:37:04 +0100

    fin cours 18 novembre

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,13 +5,30 @@ public class MovementController : MonoBehaviour
 {
     public Vector3 movementDirection;
     public float movementSpeed;
+    public Rigidbody2D body;
 
     // Update is called once per frame
     void Update()
     {
-        Move(movementDirection, movementSpeed);
+        if (body == false || body.bodyType == RigidbodyType2D.Kinematic)
+        {
+            Move(movementDirection, movementSpeed);
+        }
     }
 
+    void FixedUpdate()
+    {
+        if (body && body.bodyType == RigidbodyType2D.Dynamic)
+        {
+            MoveDynamic(movementDirection, movementSpeed);
+        }
+    }
+
+    private void MoveDynamic(Vector3 direction, float speed)
+    {
+        body.linearVelocity = direction.normalized * speed;
+    }
+    
     private void Move(Vector3 direction, float speed)
     {
         transform.position += direction.normalized * (Time.deltaTime * speed);
