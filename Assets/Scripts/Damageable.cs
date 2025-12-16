d65803cf7710c8170f2996cb520594eafab17c12
jeango <jdc@funtomata.com>
2025-12-16 12:31:42 +0100

    fin cours 16 décembre

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class Damageable : MonoBehaviour
 {
+    public GameObject deathPrefab;
+    public int startingHealth = 1;
+    
+    private int currentHealth;
+
+    void OnEnable()
+    {
+        currentHealth = startingHealth;
+    }
+    
     public void TakeDamage()
     {
         ScoreGain gainComponent = GetComponent<ScoreGain>();
@@ -9,6 +19,20 @@ public class Damageable : MonoBehaviour
         {
             gainComponent.GainScore();            
         }
-        Destroy(gameObject);
+        currentHealth -= 1;
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        if (deathPrefab != null)
+        {
+            GameObject explosionInstance = Instantiate(deathPrefab, transform.position, Quaternion.identity);
+            Destroy(explosionInstance, 1f);
+        }
+        Destroy(gameObject);        
     }
 }
--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -3,6 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+
+    void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+    
     public void RestartGame()
     {
         GameManager.NewGame();
